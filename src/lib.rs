59ee355123ab4d3554bd65105162444f91fa350d
Nikolina <niko@DESKTOP-152SJ8E.localdomain>
2025-09-06 20:26:02 +0200

    bitboard

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,36 @@ enum Color {
     Black,
 }
 
+// bitboard courtesy of https://nereuxofficial.github.io/posts/bitboard-rust/
+#[derive(PartialEq, Eq, PartialOrd, Clone, Copy, Debug, Default, Hash)]
+pub struct BitBoard(pub u64);
+
+#[derive(Debug, Clone, Hash, PartialEq, Eq)]
+pub struct Position{
+    /// Board for each side
+    bb_sides: [BitBoard; 2],
+    // BitBoards for all pieces and each side
+    bb_pieces: [[BitBoard; 6]; 2],
+}
+
+pub struct Sides;
+impl Sides {
+    pub const WHITE: usize = 0;
+    pub const BLACK: usize = 1;
+}
+
+pub struct Pieces;
+impl Pieces{
+    pub const PAWN: usize = 0;
+    pub const BISHOP: usize = 1;
+    pub const KNIGHT: usize = 2;
+    pub const ROOK: usize = 3;
+    pub const QUEEN: usize = 4;
+    pub const KING: usize = 5;
+}
+
+// example: let white_queens: BitBoard = position.bb_pieces[Sides::WHITE][Pieces::QUEEN]; 
+
 // chess board stored as 2d array (? enum or bitboard) ; A->H, 1->8. returns 0 if no piece, returns associated string if piece (i.e. Q = queen)
 // string expensive (boo), use enum: i.e. Piece::Queen(Color::White)
 // how to keep track white/black?
