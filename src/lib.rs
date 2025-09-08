5fc706c02be2d338f045ff5e6725c4abd59a048e
Nikolina <niko@DESKTOP-QHSCD1M.localdomain>
2025-09-08 14:29:53 +0200

    turn tracker

--- src/lib.rs
+++ src/lib.rs
@@ -18,13 +18,17 @@ enum Color {
 // error handling: Results, panics, or player-facing messages.
 // debugging with https://www.chessprogramming.org/Perft
 
-// turn tracker?
-
 // special: castling, en passant, promotion
+
+int turn = 0;
+int currentPlayer: 0; // 0 = white, 1 = black
 
 // >>>> start: player inputs location of piece player wants to move
 pub fn hasPiece(coordinate: &str) -> String {
+    // playerTracker()
     // check the two arrays
+
+    // if piece -> check piece ownership vs currentPlayer. if no -> failstate
 
     /* if String:
         piece = string 
@@ -34,6 +38,19 @@ pub fn hasPiece(coordinate: &str) -> String {
 
     // return true / false
     // if true: checkValidMoves
+}
+
+pub fn playerTracker(){
+    /* 
+    if (turn % 2 == 0) { // even
+        currentPlayer = black; (notated as 1?)
+    }else { // uneven -- game starts at 1, i.e. uneven
+        current player = white; ()
+    } */
+}
+
+pub fn turnTracker() {
+    // turn++;
 }
 
 prv fn checkValidMoves(piece){
@@ -55,6 +72,7 @@ fn isValidMove {
 
 fn makeMove {
     /*
+    turnTracker();
     mutable access to board
     register the move into the board array (previous position -> 0; new position is filled with type i.e. 0 -> Q)
     then, check for checkmate (and other states?)    
